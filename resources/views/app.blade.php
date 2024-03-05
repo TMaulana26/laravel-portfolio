95ca0977f69b3e34bc5e99aa2740135d3c57be1c
TMaulana26 <2010631250078@student.unsika.ac.id>
2024-03-05 13:20:01 +0700

    update: app.blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>@yield('title', 'Taufik M')</title>
-    <link rel="stylesheet" href="{{secure_asset ('./build/assets/app.css')}}">
+    <link rel="stylesheet" href="{{secure_asset ('./build/app.css')}}">
     {{-- @vite("resources/css/app.css") --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     @livewireStyles
@@ -31,7 +31,7 @@
 
     @livewireScripts
     {{-- @vite('resources/js/app.js') --}}
-    <script type="module" src="{{ secure_asset('./build/assets/app2.js') }}"></script>
+    <script type="module" src="{{ secure_asset('./build/app2.js') }}"></script>
 </body>
 
 </html>
